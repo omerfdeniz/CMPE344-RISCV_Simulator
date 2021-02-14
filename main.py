88827063744cb510a6641b51f28746db95c4af2e
Ömer Faruk Deniz <omerfaruk.deniz@gmail.com>
2021-02-14 05:17:51 +0300

    deleted comments

--- main.py
+++ main.py
@@ -4,15 +4,3 @@ from simulator import Simulator
 program_path = "program.txt"
 simulator = Simulator(program_path)
 simulator.run()
-
-"""
-def do():
-    print("in do")
-    print(4)
-a = 5
-print(a)
-a = 10
-do()
-b = 7
-print(a,b)
-"""
