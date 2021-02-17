afc802059a060bec0ebe6e72adcf25826ecee3c0
Ömer Faruk Deniz <omerfaruk.deniz@gmail.com>
2021-02-17 23:20:17 +0300

    activated the argument reading from the command line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 from simulator import Simulator
-#program_path = sys.argv[1]
-program_path = "program.txt"
+program_path = sys.argv[1]
 simulator = Simulator(program_path)
 simulator.run()
